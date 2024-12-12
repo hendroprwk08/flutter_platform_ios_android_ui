0b431b85886311e689cf3d6f55680a798d44a374
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2024-12-12 08:45:56 +0700

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,12 +10,135 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Center(child: Text ('Flutter Demo Home Page')),
+      home: const MyHomePage(title: 'Flutter Platform'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late String nama;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: Platform.isIOS
+            ? CupertinoNavigationBar(
+                middle: Text(widget.title),
+              )
+            : AppBar(
+                backgroundColor: Colors.cyan,
+                title: Text(
+                  widget.title,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Platform.isIOS
+                  ? CupertinoTextField(
+                      placeholder: 'nama Lengkap',
+                      placeholderStyle:
+                          const TextStyle(color: CupertinoColors.inactiveGray),
+                      prefix: const Icon(CupertinoIcons.person),
+                      prefixMode: OverlayVisibilityMode.always,
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      onChanged: (value) => {
+                        nama = value
+                      },
+                    )
+                  : TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        hintText: 'Siapa namamu?',
+                        labelText: 'Nama Lengkap',
+                      ),
+                      onChanged: (value) => {
+                        nama = value
+                      },
+                    ),
+              const SizedBox(height: 20.0),
+              Platform.isIOS
+                  ? CupertinoButton.filled(
+                      child: const Text('Simpan'),
+                      onPressed: () {
+                        showPlatformDialog(nama);
+                      })
+                  : ElevatedButton(
+                      child: const Text('Simpan'),
+                      onPressed: () {
+                        showPlatformDialog(nama);
+                      },
+                    )
+            ],
+          ),
+        ));
+  }
+
+  Future showPlatformDialog(String nama) async {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+          context: context,
+          builder: (context) {
+            return CupertinoAlertDialog(
+              title: const Text('Namaku'),
+              content: Text(nama),
+              actions: [
+                CupertinoDialogAction(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Batal'),
+                ),
+                CupertinoDialogAction(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          }
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Namaku'),
+            content: Text(nama),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Batal'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
